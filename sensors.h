e0fe273aec88989d0b28f6fa8050ad0b436a08f7
Emiliano Castro <93452814+Humol-e@users.noreply.github.com>
2024-06-29 02:34:10 -0600

    Update sensors.h
    modificado todo a float o int

--- sensors.h
+++ sensors.h
@@ -3,18 +3,18 @@ struct SensorData {
     float temperatura;
     float humedad;
     float presion;
-    double latitud;
-    double longitud;     
-    double altitud;
+    float latitud;
+    float longitud;     
+    float altitud;
     int estadoFC51;
     int luminosidad;
     float aceleracionX;
     float aceleracionY;
     float aceleracionZ;
     float NivelAgua;
-    double giroX;
-    double giroY;
-    double giroZ;
+    float giroX;
+    float giroY;
+    float giroZ;
   
 
 
@@ -22,7 +22,7 @@ struct SensorData {
         return "Temperatura: " + String(temperatura) + " *C\n" +
                "Humedad: " + String(humedad) + " %\n" +
                "Presion: " + String(presion) + " hPa\n" +
-               "Luz: " + String(luz) + " lux\n" +
+               "Luz: " + String(luminosidad) + " lux\n" +
                "GPS: " + "Lat: " + String(latitud) + " Long: " + String(longitud) + " Alt: " + String(altitud) + " m\n" +
                "Aceleracion: " + "X: " + String(aceleracionX) + " Y: " + String(aceleracionY) + " Z: " + String(aceleracionZ) + " m/s^2\n" +
                "Nivel del Agua : " + String(NivelAgua) + " cm\n" +
